e84b052c1e2504da1aa262a56e9a7bae41ba1fa0
lazaroalexadre <lazaroalexandre2022@gmail.com>
2026-01-10 22:32:48 -0300

    feat #2: fazendo o logout

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -10,7 +10,7 @@ sealed class AppRoutes {
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashPage()),
       GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
-      GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+      GoRoute(path: '/home', builder: (context, state) => HomePage()),
     ],
   );
 }
