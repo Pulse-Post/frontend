58bf7de70ebdd744284ff3a5d344c9eaab7029e8
lazaroalexadre <lazaroalexandre2022@gmail.com>
2026-01-14 11:44:47 -0300

    fix: removendo o comentário em dividerDetafult e imports desnecessárias, além de criar adicionar uma listagem de posts quando sair da página de profile

--- uikit/lib/atomic/atoms/divider/divider_default.dart
+++ uikit/lib/atomic/atoms/divider/divider_default.dart
@@ -1,4 +1,4 @@
-/* // ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:uikit/uikit.dart';
 
@@ -19,4 +19,3 @@ class DividerDefault extends StatelessWidget {
     );
   }
 }
- */
